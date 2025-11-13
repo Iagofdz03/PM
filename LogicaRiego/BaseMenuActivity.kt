20a57556fc8c647f92f1e53ffc2e30c26cfd5fb5
Iagofdz03 <iagofzdz@gmail.com>
2025-11-13 13:22:15 +0100

    Create BaseMenuActivity.kt

---
+++ LogicaRiego/BaseMenuActivity.kt
@@ -0,0 +1,10 @@
+package com.example.smartgarden
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
