82f843b0b5a26779793eb0f97e8d96571ad2d359
Iagofdz03 <iagofzdz@gmail.com>
2025-11-21 10:00:07 +0100

    Create MainActivity.kt

---
+++ BasicActivity6/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.dashboardpro
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.dashboardpro.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Toolbar
+        setSupportActionBar(binding.toolbar)
+
+        // NavController + AppBar
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // FAB con Snackbar
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "FAB pulsado: abriendo notificación…", Snackbar.LENGTH_LONG)
+                .setAnchorView(R.id.fab)
+                .show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+
+            // Opción "Información"
+            R.id.action_info -> {
+                Toast.makeText(this, "Menú de información pulsado", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
